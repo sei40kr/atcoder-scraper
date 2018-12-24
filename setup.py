23bdbfa5cc130b7158f0b8684efcd46b8aa46c2e
Seong Yong-ju <sei40kr@gmail.com>
2018-12-24 18:43:30 +0900

    Add module information

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from setuptools import setup
 
 setup(
+    name="atcoder-scraper",
+    version="1.0.0",
     install_requires=["requests", "beautifulsoup4"],
+    extra_require={"develop": ["pylint", "yapf"]},
     entry_points={"console_scripts": ["atcoder-scraper = app:main"]})
